fdf0cb1c1b67c1cc2b96843ddcf2c32e243e3d47
MOUTTE <corentinmoutte@gmail.com>
2023-12-06 14:25:59 +0100

    MP24T3-12-creation-de-main-cpp

--- interfaceJoueurs.cpp
+++ interfaceJoueurs.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "interfaceJoueurs.h"
 
+InterfaceJoueurs::InterfaceJoueurs() : lettreProposee('\0'), tentativesRestantes(0)
+{
+}
+
 int InterfaceJoueurs::getTentativeRestantes()
 {
     return tentativesRestantes;
