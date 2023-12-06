2d51654cca53967f65997e4b1501d43d4980c891
nmdoiouhoma <nakibmdoiouhoma.btssn@gmail.com>
2023-12-06 13:47:12 +0100

    MP24T3-15-relation-entre-les-classes-joueurs-et-jeu-pendu

--- jeuPendu.h
+++ jeuPendu.h
@@ -1,19 +1,15 @@
 #ifndef JEU_PENDU_H
-
 #define JEU_PENDU_H
 #define ESSAI 9
 
 #include <string>
 #include <vector>
 #include "dictionnaire.h"
-
 #include "joueur.h"
-#define ESSAI 9
 
 class Joueur;
 class Dictionnaire;
 class interfaceJoueurs;
-
 class JeuPendu
 {
   private:
@@ -35,9 +31,9 @@ class JeuPendu
     bool        verifierMot();
     void        choisirMot();
     void        jouer();
-
-    void relationDictionnaire(Dictionnaire* dictionnaire);
-    void relationInterfaceJoueurs(interfaceJoueurs* monInterface);
+    void        relationDictionnaire(Dictionnaire* dictionnaire);
+    void        relationInterfaceJoueurs(interfaceJoueurs* monInterface);
+    void        relationJoueur(Joueur* monJoueur);
 };
 
 #endif // JEU_PENDU_H
